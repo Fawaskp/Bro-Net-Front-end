6e8882d4b665e18f69d2ccda905e7e19406182f0
Fawas <fawaskp1010@gmail.com>
2023-07-13 22:41:28 +0530

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
     <header className="border-b border-gray-300">
         <nav className="container mx-auto flex justify-between items-center py-4">
             <div className="logo-side">
-                <h3 className="text-xl text-indigo-600 font-bold tracking-wider uppercase">BROCAMP</h3>
+                <h3 className="text-xl text-indigo-600 font-bold tracking-wider uppercase">BRO-NET</h3>
             </div>
         </nav>
     </header>
