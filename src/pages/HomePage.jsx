e18e3e443665336c752af70db9271a61aeabb12d
Fawas <fawaskp1010@gmail.com>
2023-07-13 22:36:13 +0530

    initial setup

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+    <header className="border-b border-gray-300">
+        <nav className="container mx-auto flex justify-between items-center py-4">
+            <div className="logo-side">
+                <h3 className="text-xl text-indigo-600 font-bold tracking-wider uppercase">BROCAMP</h3>
+            </div>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default HomePage
